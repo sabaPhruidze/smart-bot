3ba5fd5f6e56729d31e78bfce103bc282155fa56
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-19 18:09:26 +0400

    in mobile it will be centered in bigger size on the right side

--- components/ChatWidget/ChatWidget.tsx
+++ components/ChatWidget/ChatWidget.tsx
@@ -2,7 +2,7 @@ import Launcher from "./Launcher";
 
 const ChatWidget = () => {
   return (
-    <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end gap-4 ">
+    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-50 flex flex-col items-center gap-4 sm:left-auto sm:translate-x-0 sm:right-6">
       <Launcher />
     </div>
   );
